accdf10514e9af9e2d1ad2477a53c88c0d6afe6b
yjb23tech <yjb23tech@gmail.com>
2023-07-10 10:29:58 +0100

    Working with Scraps

---
+++ scraps_1.py
@@ -0,0 +1,15 @@
+var_1 = 10 
+var_2 = 19 
+
+total_sum = var_1 + var_2 
+
+print(total_sum)
+
+def func_sum(var_x, var_y):
+    var_z = var_x + var_y 
+    return var_z 
+
+test_sum = func_sum(100, 150)
+print(test_sum)
+
+
