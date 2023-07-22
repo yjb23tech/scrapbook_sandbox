03929e1817ef3d2aeaa61187358f8d74aa634a22
yjb23tech <yjb23tech@gmail.com>
2023-07-22 19:03:05 +0100

    War of Worlds

--- scraps_xxii_vii.py
+++ scraps_xxii_vii.py
@@ -1,1 +1,15 @@
-print("Warm Ups")
+#print("\nWarm Ups\n")
+
+arr_heroes_marvel = ['Iron Man', 'Spider Man', 'Mr Fantastic', 'Black Panther', 'Storm'] 
+arr_heroes_dc = ['Batman', 'Super Man', 'Cyborg', 'The Flash', 'Wonder Woman'] 
+
+print("\nFrom the Marvel Universe:\n")
+for hero_marvel in arr_heroes_marvel:
+    print(f"You can choose: {hero_marvel}")
+
+print("\nAnd from the DC Universe:\n")
+for hero_dc in arr_heroes_dc:
+    print(f"You can choose: {hero_dc}")
+
+
+
