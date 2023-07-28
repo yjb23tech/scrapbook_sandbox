acff427451abcc35d7ef63ef54f846e7d82242cd
yjb23tech <yjb23tech@gmail.com>
2023-07-28 14:22:59 +0100

    Tag Team

--- xviii_vii.py
+++ xviii_vii.py
@@ -1,2 +1,20 @@
-print("I apologise; I need to be better")
+#print("I apologise; I need to be better")
+
+arr_naruto_heroes = ['Naruto', 'Sasuke', 'Raikage', 'Hokage', 'Gai']
+arr_onepiece_heroes = ['Luffy', 'Blackbeard', 'Red Shanks', 'Garp', 'Oden']
+
+print(" ")
+for hero in arr_naruto_heroes:
+    print(f"{arr_naruto_heroes.index(hero) + 1}. {hero}")
+
+print(" ")
+
+(" ")
+for i, hero in enumerate(arr_onepiece_heroes, 1):
+    print(f"{i}. {hero}")
+
+print(" ")
+
+
+
 
