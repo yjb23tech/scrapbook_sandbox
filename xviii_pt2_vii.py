d8251ec830fe0d4e5e04358923aae6962396d356
yjb23tech <yjb23tech@gmail.com>
2023-07-28 21:42:11 +0100

    Building

--- xviii_pt2_vii.py
+++ xviii_pt2_vii.py
@@ -1,1 +1,16 @@
 print("Game On")
+
+arr_inventory = ['Dagger', 'Gold(5)', 'Crusty Bread', 'Iron(3)']
+arr_ui_options = ['N for North', 'E for East', 'S for South', 'W for West', 'I for Inventory'] 
+
+def play():
+
+    print("\nWhat you like to do? Choose from the options below:\n")
+    i = 1
+    for option in arr_ui_options:
+        print(f"{i}. Press {option}")
+        i += 1
+    print(" ")
+
+ 
+play()
