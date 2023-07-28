1a4c3c38af74a580f1e46703e3b4c792a9b68926
yjb23tech <yjb23tech@gmail.com>
2023-07-28 14:28:40 +0100

    2 on 2s

--- xviii_vii.py
+++ xviii_vii.py
@@ -2,6 +2,8 @@
 
 arr_naruto_heroes = ['Naruto', 'Sasuke', 'Raikage', 'Hokage', 'Gai']
 arr_onepiece_heroes = ['Luffy', 'Blackbeard', 'Red Shanks', 'Garp', 'Oden']
+arr_bleach_heroes = ['Ichigo', 'Iruka', 'Gin', 'Chad', 'Inoue']
+arr_deathnote_heroes = ['Light', 'Ryokuza', 'Near', 'Mello', 'Ryuk'] 
 
 print(" ")
 for hero in arr_naruto_heroes:
@@ -15,6 +17,19 @@ for i, hero in enumerate(arr_onepiece_heroes, 1):
 
 print(" ")
 
+print(" ")
+for i in range(len(arr_bleach_heroes)):
+    print(f"{i+1}. {arr_bleach_heroes[i]}")
+
+print(" ")
+
+i = 0 
+print(" ")
+for hero in arr_deathnote_heroes:
+    print(f"{i+1}. {arr_deathnote_heroes[i]}")
+    i += 1 
+
+print(" ")
 
 
 
