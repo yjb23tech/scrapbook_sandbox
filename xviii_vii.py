9e6f301849663f83f1bf5b7b414aaf9f3ac909c6
yjb23tech <yjb23tech@gmail.com>
2023-07-28 14:34:41 +0100

    FMA the Goat

--- xviii_vii.py
+++ xviii_vii.py
@@ -4,6 +4,7 @@ arr_naruto_heroes = ['Naruto', 'Sasuke', 'Raikage', 'Hokage', 'Gai']
 arr_onepiece_heroes = ['Luffy', 'Blackbeard', 'Red Shanks', 'Garp', 'Oden']
 arr_bleach_heroes = ['Ichigo', 'Iruka', 'Gin', 'Chad', 'Inoue']
 arr_deathnote_heroes = ['Light', 'Ryokuza', 'Near', 'Mello', 'Ryuk'] 
+arr_fma_heroes = ['Edward', 'Alphonse', 'Mustang', 'Homonculus', 'Armstrong']
 
 #Running a single for loop an on array and also using the index location information: my way
 print(" ")
@@ -29,6 +30,13 @@ print(" ")
 for hero in arr_deathnote_heroes:
     print(f"{i+1}. {arr_deathnote_heroes[i]}")
     i += 1 
+print(" ")
+
+i = 1 
+print(" ")
+for hero in arr_fma_heroes:
+    print(f"{i}. {hero}")
+    i += 1
 print(" ")
 
 
