42e510946c1006e9648564ce49393076599fcd42
yjb23tech <yjb23tech@gmail.com>
2023-08-10 18:58:46 +0100

    OG Chuka

--- x_viii.py
+++ x_viii.py
@@ -1,1 +1,25 @@
-print("Welcome Back")
+#print("Welcome Back")
+
+arr_inventory_items = ['Gold(5)', 'Broad Sword', 'Medicine', 'Iron', 'Potions'] 
+arr_action_options = ['N to travel North', 'E to travel East', 'S to travel South', 'W to travel West', 'I to access the items in your Inventory', 'Q to save and Quit the game'] 
+
+def get_user_input():
+    return input("\nWhat would you like to do next Captain?\n")
+
+def play():
+    bool_game_is_on = True
+    print("\nWelcome to Treasure Island! Enter if you dare -_____-\n")
+
+    while (bool_game_is_on):
+        for i, action in enumerate(arr_action_options, 1):
+            print(f"{i}. Press {action}")
+
+        ui_action = get_user_input()
+
+        if ui_action == 'A':
+            print("\nGood so far\n")
+        else:
+            print("\nYou have now saved and quit the game: well done Captain\n")
+            bool_game_is_on = False 
+
+play()
