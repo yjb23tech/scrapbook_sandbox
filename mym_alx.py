430b7fae09236fa0badb8100320d267adf0cde29
yjb23tech <yjb23tech@gmail.com>
2023-08-27 22:35:23 +0100

    Homework lol

---
+++ mym_alx.py
@@ -0,0 +1,8 @@
+def sum_of_squares(int_x, int_y):
+    sum_of_squares_result = (int_x * int_x) + (int_y * int_y)
+    return sum_of_squares_result 
+
+test_output = sum_of_squares(3, 7) 
+
+print(f"The result of the sum of the squares of 3 and 7 is: {test_output}")
+
