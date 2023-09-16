87cc2bf549e89f893f0c408c1dc74e436fb41a6d
yjb23tech <yjb23tech@gmail.com>
2023-09-16 20:16:04 +0100

    Live Life

--- scraps_sep_16.py
+++ scraps_sep_16.py
@@ -1,1 +1,8 @@
 print("Thank You For Everything")
+
+arr_marvel_heroes = ['Black Panther', 'Spiderman', 'Iron Man'] 
+
+for hero in arr_marvel_heroes:
+    print(f"Welcome to Marvel vs DC {hero}")
+
+
