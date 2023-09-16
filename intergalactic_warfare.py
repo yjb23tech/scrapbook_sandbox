d4e37e4b318267b9ac80695673643ff13e971c3e
yjb23tech <yjb23tech@gmail.com>
2023-09-16 22:03:12 +0100

    Learning

--- intergalactic_warfare.py
+++ intergalactic_warfare.py
@@ -7,6 +7,8 @@ MAX_WARRIOR_LIST_SIZE = 5
 
 print("It's time to build your combat list! Choose your heroes:\n")
 
+#This function accepts an empty arr as it's param; using a 'while' loop, loop will run on condition LEN aka SIZE of arr is < 5
+#ui input is taken and appended to arr, increasing it in size by +1 each turn
 def ui_build_warrior_list(arr_warriors):
 
     while (len(arr_warriors) < MAX_WARRIOR_LIST_SIZE):
@@ -14,13 +16,14 @@ def ui_build_warrior_list(arr_warriors):
         arr_warriors.append(ui_warrior_input)
         print(" ")
 
-
+#This function accepts an arr; uses a 'for' loop in conjunction with 'enumerate' func to loop through the contents of arr and print item stored in corresponding index location
 def ui_print_warrior_list(arr_warriors):
 
     for x, warrior in enumerate(arr_warriors, 1):
         print(f"Please welcome to the stage {x}. {warrior}")
 
-
+#Len func on arr returns a integer reflecting size of arr i.e. 5; Range func returns an object which maps to the index locations in the arr using zero indexing so '5' becomes i.e. [0, 1, 2, 3, 4] 
+#And so loop will run 5 times; each index location value is mapped to 'x'; needs increment to be intelligible
 def ui_print_warrior_list_alt(arr_warriors):
 
     for x in range(len(arr_warriors)):
