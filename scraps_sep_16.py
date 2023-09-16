f27bd1175602b8f28a53214ed6d697727b629ace
yjb23tech <yjb23tech@gmail.com>
2023-09-16 20:22:22 +0100

    Surprise

--- scraps_sep_16.py
+++ scraps_sep_16.py
@@ -2,6 +2,7 @@
 
 arr_marvel_heroes = ['Black Panther', 'Spiderman', 'Iron Man'] 
 arr_dc_heroes = ['Super Man', 'Batman', 'Wonder Woman'] 
+arr_bleach_heroes = ['Ichigo', 'Kenpachi', 'Yammamato'] 
 
 print(" ")
 
@@ -13,4 +14,10 @@ print(" ")
 for hero in arr_dc_heroes: 
     print(f"Salutations! Welcome to Marvel vs DC {hero}")
 
+print("\nWe have a surprise addition to the combat universe!\n")
+
+for x, hero in enumerate(arr_bleach_heroes, 1):
+    print(f"Witness the arrival of {x}. {hero} from the Bleach Universe")
+
+print(" ")
 
