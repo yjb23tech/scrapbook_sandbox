37c5bd71ddf7a916c99de7e74078706d92b4bba4
yjb23tech <yjb23tech@gmail.com>
2023-09-16 20:18:11 +0100

    Comic Books

--- scraps_sep_16.py
+++ scraps_sep_16.py
@@ -1,8 +1,16 @@
-print("Thank You For Everything")
+#print("Thank You For Everything")
 
 arr_marvel_heroes = ['Black Panther', 'Spiderman', 'Iron Man'] 
+arr_dc_heroes = ['Super Man', 'Batman', 'Wonder Woman'] 
+
+print(" ")
 
 for hero in arr_marvel_heroes:
     print(f"Welcome to Marvel vs DC {hero}")
+
+print(" ")
+
+for hero in arr_dc_heroes: 
+    print(f"Salutations! Welcome to Marvel vs DC {hero}")
 
 
