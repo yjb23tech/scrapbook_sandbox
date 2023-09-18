9b4b420e2cc50f7af59446d399a78f46f987bdbc
yjb23tech <yjb23tech@gmail.com>
2023-09-18 15:45:35 +0100

    Cleaning things up

--- nba_finals.py
+++ nba_finals.py
@@ -4,6 +4,8 @@ arr_nba_east_coast_team = []
 arr_nba_west_coast_team = [] 
 arr_nba_positions = ['Point Guard', 'Shooting Guard', 'Small Forward', 'Power Forward', 'Center'] 
 
+bool_game_is_on = True
+
 #Need a function for building a team of 5 starting players
 
 def build_nba_team(arr_nba_team, coast_selection, arr_positions):
@@ -23,8 +25,32 @@ def build_nba_team(arr_nba_team, coast_selection, arr_positions):
 
     print(f"Let's goooooo {coast_selection.upper()} Coast team!\n")
 
-build_nba_team(arr_nba_east_coast_team, "East", arr_nba_positions)
-build_nba_team(arr_nba_west_coast_team, "West", arr_nba_positions)
+def play():
+
+    print("\nWelcome to the NBA finals! Which team would you like to build first? East or West?\n")
+    ui_coast_selection = input("To build the East Coast team, type in E-A-S-T or to build the West Coast team type in W-E-S-T:\n")
+
+    if (ui_coast_selection.upper()) == 'EAST':
+
+        print(f"You have chosen to build the East Coast team; let's go!")
+        build_nba_team(arr_nba_east_coast_team, ui_coast_selection, arr_nba_positions)
+
+        print("\nAnd now you must build the West Coast team; let's go!\n")
+        build_nba_team(arr_nba_west_coast_team, "West", arr_nba_positions)
+
+    elif (ui_coast_selection.upper()) == 'WEST':
+
+        print(f"You have chosen to build the {ui_coast_selection.upper} Coast team; let's go!")
+        build_nba_team(arr_nba_west_coast_team, ui_coast_selection, arr_nba_positions)
+
+        print("\nAnd now you must build the East Coast team; let's go!\n")
+        build_nba_team(arr_nba_east_coast_team, "East", arr_nba_positions)
+
+    else:
+        print("Invalid selection; try again!")
+
+
+play()
 
 #Need a function to run as the main flow for the program i.e. play() 
 
