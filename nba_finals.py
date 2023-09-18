c9cb36663fea172795e713b70e2d8bf42289a3e2
yjb23tech <yjb23tech@gmail.com>
2023-09-18 15:07:11 +0100

    Gunna

--- nba_finals.py
+++ nba_finals.py
@@ -2,15 +2,18 @@
 
 arr_nba_east_coast_team = [] 
 arr_nba_west_coast_team = [] 
+arr_nba_positions = ['Point Guard', 'Shooting Guard', 'Small Forward', 'Power Forward', 'Center'] 
 
 #Need a function for building a team of 5 starting players
 
-def build_nba_team(arr_nba_team, coast_selection):
+def build_nba_team(arr_nba_team, coast_selection, arr_positions):
     print(f"\nYou have chosen to build your {coast_selection} Coast team; let's get started XD")
 
+    x = 0 
     while(len(arr_nba_team) < 5):
-        ui_nba_player = input(f"\nWhich player would you like to have in your {coast_selection.upper()} Coast team's starting five?\n")
+        ui_nba_player = input(f"\nWhich player would you like to have in your {coast_selection.upper()} Coast team at the {arr_nba_positions[x]} position?\n")
         arr_nba_team.append(ui_nba_player)
+        x += 1 
  
     print("\nYour team is complete; here is your team:\n")
     i = 1 
@@ -20,7 +23,7 @@ def build_nba_team(arr_nba_team, coast_selection):
 
     print(f"Let's goooooo {coast_selection.upper()} Coast team!\n")
 
-build_nba_team(arr_nba_east_coast_team, "East")
+build_nba_team(arr_nba_east_coast_team, "East", arr_nba_positions)
 
 #Need a function for printing a team of 5 starting players 
 
