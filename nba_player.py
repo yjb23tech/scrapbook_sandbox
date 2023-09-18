ae3358f513de962653851aca9aa4d866adb58195
yjb23tech <yjb23tech@gmail.com>
2023-09-18 16:49:40 +0100

    Penny Hardaway

---
+++ nba_player.py
@@ -0,0 +1,20 @@
+class NBAPlayer:
+
+    def __init__(self, str_player_name, str_player_position, int_player_attack_power, int_player_defense_power):
+
+        self.str_player_name = str_player_name
+        self.str_player_position = str_player_position
+        self.int_player_attack_power = int_player_attack_power 
+        self.int_player_defense_power = int_player_defense_power 
+
+    def __str__(self):
+
+        return (f"At the {self.str_player_position} position we have: {self.str_player_name}! Attack Power is {self.int_player_attack_power} with a defensive rating of {self.int_player_defense_power}")
+        
+
+lebron_james = NBAPlayer("LeBron James", "Small Forward", 100, 99)
+
+print("")
+print(lebron_james)
+print("")
+
