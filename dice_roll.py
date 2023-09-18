9b1e27fdb51a8a25a83346e7f780e6ef36a3f4b9
yjb23tech <yjb23tech@gmail.com>
2023-09-18 19:06:24 +0100

    Using random mod

--- dice_roll.py
+++ dice_roll.py
@@ -1,6 +1,23 @@
 import random
-number = random.randint(1, 6)
 
-print("Working with imaginary die")
-print(number)
+def dice_roll():
+    number = random.randint(1, 6)
+    return number
+
+bool_game_is_on = True
+
+while (bool_game_is_on):
+
+    ui_game_decision = input("\nWould you like to roll the die?\n")
+    if (ui_game_decision.upper()) == 'NO':
+        print("Thank you for playing; you have now quit the game")
+        bool_game_is_on = False
+
+    else:    
+        print("Roll the die!\n")
+        dice_roll_result = dice_roll()
+        print(f"You rolled a {dice_roll_result}")
+
+
+
 
