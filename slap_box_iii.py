979ffbe7581285e4a7d34ebca816f6ddc01b0b16
yjb23tech <yjb23tech@gmail.com>
2023-09-19 14:59:30 +0100

    10_000 hours

--- slap_box_iii.py
+++ slap_box_iii.py
@@ -12,7 +12,7 @@ class Player:
         self.int_player_def_power = 1  
 
     def __str__(self):
-        return (f"All the way from {self.str_player_location} we have {self.str_player_name}! He's only {self.int_player_age} years old but be warned, he's deadly in the ring!")
+        return (f"\nAll the way from {self.str_player_location} we have {self.str_player_name}! He's only {self.int_player_age} years old but be warned, he's deadly in the ring!\n")
 
     def int_atk_power(self):
         self.int_player_atk_power = random.randint(1, 20)
@@ -33,5 +33,8 @@ def str_set_player_location():
 def int_set_player_age():
     return (int(input(f"\nAnd how old are you Warrior?\n")))
 
+player_1 = Player(str_set_player_name(), str_set_player_location(), int_set_player_age())
+
+print(player_1)
 
 print("How far can you take it?")
