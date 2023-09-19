808d8094a59ad4aa86deb66013aeb32847b02e11
yjb23tech <yjb23tech@gmail.com>
2023-09-19 17:01:33 +0100

    Hopefully this works

---
+++ random_text_gen.py
@@ -0,0 +1,8 @@
+#The Black Panther lands a .... on The Amazing Spider Man
+#In a shocking turn of events Iron Man has .... the attack from Thor! 
+
+
+arr_atk_wins_text = ['shattering blow', 'devastating shot to the liver', 'gut wrenching strike to the solar plexus', 'vicious slash', 'critical slam'] 
+arr_def_wins_text = ['managed to successfully evade', 'intercepted and then parried', 'deflected and reversed', 'managed to block', 'defend against'] 
+
+
