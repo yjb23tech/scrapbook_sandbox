16264eb42d121456ffd79164accbca2617d41b6b
yjb23tech <yjb23tech@gmail.com>
2023-09-19 12:35:41 +0100

    More and More

--- slap_box_ii.py
+++ slap_box_ii.py
@@ -1,1 +1,8 @@
+#Desired features
+#Dice roll to determine whether Player 1 or Player 2 gets to strike
+#Another probability event to determine whether attacking player is successful or not 
+#Attack power randomly set to model differences in striking power 
+
 print("Welcome to Slap Box ii")
+
+
