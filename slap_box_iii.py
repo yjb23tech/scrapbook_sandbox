ffbd752ddec5bab3cd5ea4c197d227ed5a585b0f
yjb23tech <yjb23tech@gmail.com>
2023-09-19 15:24:50 +0100

    Faster

--- slap_box_iii.py
+++ slap_box_iii.py
@@ -2,6 +2,8 @@
 #Can I create a func which actually models the pvp? Would accept objects
 #Might consider atk power ups, def boosts, magic and spells...
 
+import random
+
 class Player:
 
     def __init__(self, str_player_name, str_player_location, int_player_age):
@@ -33,6 +35,10 @@ def str_set_player_location():
 def int_set_player_age():
     return (int(input(f"\nAnd how old are you Warrior?\n")))
 
+#Functions 
+def int_dice_roll():
+    return (int(random.randint(1,6)))
+
 player_1 = Player(str_set_player_name(), str_set_player_location(), int_set_player_age())
 print(player_1)
 
@@ -43,9 +49,12 @@ print("\nWelcome to Paradise Lost - or Hell in Heaven as our friends like to cal
 
 loop_counter = 1 
 
-while (loop_counter < 6):
+while (loop_counter < 12):
 
     print(f"Round {loop_counter}! Let's fight!")
+    dice_roll_result = int_dice_roll() 
+    print(f"Current score on the die is {dice_roll_result}\n")   
+
     loop_counter += 1 
 
 print(" ")
