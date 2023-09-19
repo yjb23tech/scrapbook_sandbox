2db9b8d822fb1fe42cdbce5fe5056ba45dea10bd
yjb23tech <yjb23tech@gmail.com>
2023-09-19 13:28:44 +0100

    Civil War

--- slap_box_ii.py
+++ slap_box_ii.py
@@ -9,7 +9,9 @@ class Player:
     def __init__(self, str_player_name, str_player_location):
         self.str_player_name = str_player_name
         self.str_player_location = str_player_location
-        self.int_player_hp = 100 
+        self.int_player_hp = 100
+        self.int_player_atk_power = 1
+        self.int_player_def_power = 1 
 
     def __str__(self):
         return (f"Hailing all the way from {self.str_player_location} please put your hands together and give a warm welcome to {self.str_player_name}!")
@@ -37,16 +39,22 @@ player_2 = Player(set_player_name(), set_player_location())
 print(" ")
 print(player_1)
 print(player_2)
-print(" ")
 
 loop_counter = 1 
 
 while (loop_counter < 6):
 
-    print(f"Let round {loop_counter} begin!")
+    print(f"\nLet round {loop_counter} begin!\n")
 
-    print(f"{player_1.str_player_name} has an attack power of {player_1.int_atk_power()}")
-    print(f"{player_2.str_player_name} has a defensive power of {player_2.int_def_power()}\n")
+    print(f"{player_1.str_player_name} has an attack power of {player_1.int_atk_power()}!")
+    print(f"{player_2.str_player_name} has a defensive power of {player_2.int_def_power()}!")
+
+    if (player_1.int_player_atk_power > player_2.int_player_def_power):
+        print(f"{player_1.str_player_name} has overwhelmed {player_2.str_player_name} with tremendous force! The damage is severe!")
+    elif (player_1.int_player_atk_power < player_2.int_player_def_power):
+        print(f"{player_2.str_player_name} is a fortress! {player_1.str_player_name} cannot crack through the impenetrable defense!")
+    else:
+        print(f"The attacking force of {player_1.str_player_name} and the defensive strength of {player_2.str_player_name} are equally matched! We're in a STALEMATE XD")
 
     loop_counter += 1 
 
