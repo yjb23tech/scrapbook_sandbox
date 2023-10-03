01a55c865535a4705732b8c85f8f3275aa2b572e
yjb23tech <yjb23tech@gmail.com>
2023-10-03 19:15:36 +0100

    Still working

--- quick_draw.py
+++ quick_draw.py
@@ -1,7 +1,9 @@
 arr_map_grid_tiles_names = ['Island_1', 'Island_2', 'Island_3', 'Island_4', 'Island_5']
-arr_user_player_tiles_conquered = ['Island_4', 'Island_3']
+arr_user_player_tiles_conquered = ['Island_4', 'Island_3', 'Island_1', 'Island_5', 'Island_2']
 
 arr_tiles_still_to_be_conquered = [] 
+
+print(f"{len(arr_tiles_still_to_be_conquered)}")
 
 for tile in arr_map_grid_tiles_names:
 
@@ -10,13 +12,16 @@ for tile in arr_map_grid_tiles_names:
     else:
         arr_tiles_still_to_be_conquered.append(tile)
 
-print(" ")
-for x, tile in enumerate(arr_tiles_still_to_be_conquered, 1):
-    print(f"{x}. {tile} has yet to be conquered!")
-print(" ")
+if (len(arr_tiles_still_to_be_conquered) == 0):
+    print("\nYou have conquered all of the tiles and completed the game; congratulations XD\n")
+    #some bool now changed to break game loop so game can be exited 
+else:
+    print(" ")
+    for x, tile in enumerate(arr_tiles_still_to_be_conquered, 1):
+        print(f"{x}. {tile} has yet to be conquered!")
+    print(" ")
 
 #Should have a way of popping each conquered tile such that when you encounter a tile which hasn't been conquered, all tiles from that point on remain 
 #can then run a loop to show the tiles outstanding 
 #genius 
-
 
