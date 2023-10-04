f8e72d47356fb5d79806b25c36a57e16b1134a1f
yjb23tech <yjb23tech@gmail.com>
2023-10-04 23:11:08 +0100

    Len then range it

---
+++ quick_draw_iii.py
@@ -0,0 +1,16 @@
+arr_world_map = [
+
+    ["Dressrosa", "Skypeia", "Whole Cake Island"], 
+    ["Wano", "Marineford", "Thriller Bark"], 
+    ["Onigashima", "Zou", "Punk Hazard"]
+
+]
+
+print(" ")
+for x in range(len(arr_world_map)):
+    for y in range(len(arr_world_map)):
+        print(f"Welcome to {arr_world_map[x][y]}")
+print(" ")
+
+
+
