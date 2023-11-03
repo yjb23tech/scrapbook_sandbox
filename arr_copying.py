cc848aa004a2cb8dad5858e68ada58843b277880
yjb23tech <yjb23tech@gmail.com>
2023-11-03 23:46:36 +0000

    Tony Blundetto

---
+++ arr_copying.py
@@ -0,0 +1,13 @@
+my_food = ['Pizza', 'Chicken', 'Rice and Peas'] 
+
+print(my_food)
+
+copied_food = my_food[:]
+
+print(copied_food)
+
+my_food.append('GG')
+copied_food.append('ZZ')
+
+print(my_food)
+print(copied_food)
---
+++ negative_slicing.py
@@ -0,0 +1,5 @@
+players = ['Charles', 'Martina', 'Michael', 'Florence', 'Eli'] 
+print(players[-3:])
+
+print(players[-1])
+
