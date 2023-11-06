4a13973709289fc4aaa1df3fabb4c082b0619f43
yjb23tech <yjb23tech@gmail.com>
2023-11-06 19:50:30 +0000

    Messing around

---
+++ chp_6_crash_course.py
@@ -0,0 +1,11 @@
+gordon_gekko = {
+    'Name': "Gordon Gekko", 
+    'Age': 47, 
+    'Occupation': "Corporate Raider", 
+}
+
+for k, v in gordon_gekko.items():
+    print(f"\n{k}")
+    print(f"{v}\n")
+
+
